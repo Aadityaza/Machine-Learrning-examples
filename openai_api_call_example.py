8b1876333cb85f4b732125bb8c5136aba385d3d4
Aaditya Jha <45432533+Aadityaza@users.noreply.github.com>
2023-10-01 01:36:13 -0700

    Create openai_api_call_example.py

---
+++ openai_api_call_example.py
@@ -0,0 +1,37 @@
+ipenai_api_call_example.py
+
+def openai_extract_lab_data(text):
+    openai.api_key = os.getenv("OPENAI_API_KEY", your_api_key_here)
+    # Fine-tuned prompt for better and accurate extraction
+    prompt = [
+        {
+            "role": "system",
+            "content": """
+                  explain wou you would like gpt to respond
+            """
+
+        },
+        {
+            "role": "user",
+            "content": text
+        }
+    ]
+
+    # Define the API request with fine-tuned parameters for better quality
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=prompt,
+        temperature=0.5,
+        max_tokens=2000,  # Adjust this based on your requirement
+        top_p=1.0,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    # Extract the assistant's reply from the API response
+    assistant_reply = response['choices'][0]['message']['content']
+
+    print("gpt le k vancha: ", assistant_reply)
+    data = extract_valid_entries(assistant_reply)
+    print(data)  # Now `data_list` is a list of dictionaries containing your data
+    return data
